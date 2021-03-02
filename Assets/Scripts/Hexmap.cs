9be86940bda7b399d21b348e704a450449be0559
jonan270 <57438307+jonan270@users.noreply.github.com>
2021-03-02 23:23:14 +0100

    Made a bad function that affects an area around a center tile.

--- Assets/Scripts/Hexmap.cs
+++ Assets/Scripts/Hexmap.cs
@@ -16,8 +16,8 @@ public class Hexmap : MonoBehaviour
     int count;
 
     // Map size in terms of hexes
-    const int width = 7;
-    const int height = 7;
+    const int width = 20;
+    const int height = 20;
     
     // Hextiles is an array containing all gameobjects at index [x,y]
     public Hextile[,] hexTiles = new Hextile[width,height];
@@ -28,17 +28,11 @@ public class Hexmap : MonoBehaviour
 
     // Offset values
     float xoff = 0.8f;//0.8f;
-    //int xoff = 1;
     float zoff = 0.46f;//0.46f;
 
     void Awake()
     {
         //rayhandler.controls.Player.Tilespin.performed += ctx => Test();
-    }
-
-    void Test()
-    {
-        Debug.Log("yo");
     }
 
     // Start generates all tiles and places them in the array.
@@ -84,17 +78,69 @@ public class Hexmap : MonoBehaviour
             //hexTiles[1, 3].spinTile(); // -1 +2
             //hexTiles[1, 1].spinTile(); // -1 -1
 
-            hexTiles[0, 0].spinTile();
-            hexTiles[1, 0].spinTile();
-            hexTiles[0, 6].spinTile();
-            hexTiles[3, 6].spinTile();
+            //hexTiles[0, 0].spinTile();
+            //hexTiles[1, 0].spinTile();
+            //hexTiles[0, 6].spinTile();
+            //hexTiles[3, 6].spinTile();
+            affectRadius(5, 5, 3);
+            affectRadius(10, 12, 2);
         }
 
     }
 
+    // This function is completely unreadable. Good luck! Mvh Jonathan vid klockan 23:16
+    // Should be changed some day. Preferably before 21:00.
 
-    void areaTarget(int xcord, int ycord, int radius)
+    //TODO: Check for out of bounds
+    void affectRadius(int xCord, int yCord, int radius)
     {
-        
+        int nAffect = 0; // Number of tiles on each side of center to affect
+
+        // is center of first row consisting of 1 or 2 tiles?
+        bool symmetric;
+        if (radius % 2 == 0)
+        {
+            symmetric = true;
+            nAffect++;
+        }
+        else
+            symmetric = false;
+
+        // Affect a growing amount of points around centerpoints.
+        for(int xi = -radius; xi <= radius; xi++)
+        {
+            if (xi <= 0)
+                nAffect += 1; 
+            else
+                nAffect -= 1;
+
+            int range = nAffect / 2 + (radius - 1) / 2;
+
+            if (symmetric)
+            {   
+                for(int yi = -range; yi <= range; yi++)
+                {
+                    hexTiles[xCord + xi, yCord + yi].spinTile();
+                }
+                symmetric = false;
+            }
+            else
+            {
+                // Weird correction necessary? I dont know.
+                int correction = 0;
+                if (xCord % 2 != 0)
+                    correction = 1;
+
+                for (int yi = -range; yi <= range; yi++)
+                {
+                    hexTiles[xCord + xi, yCord + yi - correction].spinTile();
+                    if(yi > 0)
+                    {
+                        hexTiles[xCord + xi, yCord + yi + 1 - correction].spinTile();
+                    }
+                }
+                symmetric = true;
+            }
+        }
     }
 }
