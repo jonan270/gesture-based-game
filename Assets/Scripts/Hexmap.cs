20353cf99daa51251f5ed0cf84e27c9b2516b8e5
jonan270 <57438307+jonan270@users.noreply.github.com>
2021-03-02 15:15:18 +0100

    Cleanup

--- Assets/Scripts/Hexmap.cs
+++ Assets/Scripts/Hexmap.cs
@@ -3,9 +3,9 @@ using System.Collections.Generic;
 using UnityEngine;
 
 /*
- * Hexmap generates and keeps track of all generated map tiles
- * 
- * 
+ * Hexmap generates and keeps track of all generated map tiles.
+ * It tells each tile to perform certain via function calls to
+ * each specific tile.
  */
 
 
@@ -13,24 +13,37 @@ using UnityEngine;
 
 public class Hexmap : MonoBehaviour
 {
+    int count;
+
     // Map size in terms of hexes
     const int width = 11;
     const int height = 22;
     
     // Hextiles is an array containing all gameobjects at index [x,y]
-    public GameObject[,] hexTiles = new GameObject[width,height];
+    public Hextile[,] hexTiles = new Hextile[width,height];
 
     // Each tile is a hexPrefab
-    public GameObject hexPrefab;
+    public Hextile hexPrefab;
+    public Raycasthandler rayhandler;
 
     // Offset values
     float xoff = 0.8f;
     float zoff = 0.46f;
 
+    void Awake()
+    {
+        //rayhandler.controls.Player.Tilespin.performed += ctx => Test();
+    }
+
+    void Test()
+    {
+        Debug.Log("yo");
+    }
+
     // Start generates all tiles and places them in the array.
     void Start()
     {
-        GameObject instantiated;
+        Hextile instantiated;
         for (int i = 0; i < width; i++)
         {
             for (int j = 0; j < height; j++)
@@ -47,15 +60,37 @@ public class Hexmap : MonoBehaviour
                 }
                 instantiated.transform.localScale = Vector3.one;
                 hexTiles[i, j] = instantiated;
-                Debug.Log(hexTiles[i, j]);
+                //Debug.Log(hexTiles[i, j]);
             }
         }
+        //hexTiles[2, 2].spinTile();
     }
 
 
     // Update is called once per frame
     void Update()
     {
-        
+        // Example: rotate tile at index 2,2 every 1000 frames.
+        ++count;
+        if (count % 1000 == 0)
+        {
+            hexTiles[2, 2].spinTile();
+            hexTiles[2, 1].spinTile(); // +0 -1
+            hexTiles[2, 0].spinTile(); // +0 -2
+            hexTiles[2, 3].spinTile(); // +0 +1
+            hexTiles[2, 4].spinTile(); // +0 +2
+
+            hexTiles[1, 3].spinTile(); // -1 +2
+            hexTiles[1, 1].spinTile(); // -1 -1
+
+            //hexTiles[2, 3].spinTile();
+            //hexTiles[2, 1].spinTile();
+        }
+
+    }
+
+    void areaTarget(int radius)
+    {
+
     }
 }
--- Assets/Scripts/Hextile.cs
+++ Assets/Scripts/Hextile.cs
@@ -2,24 +2,48 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+/*
+ * Hextile contains basic information about a specific tile and provides
+ * functions for modifying that specific tile
+ */
+
 public class Hextile : MonoBehaviour
 {
-    public Vector3 position;
+    public bool spin; // Should the tile be rotating?
+    int angleCount = 0; // The angle of the tile during rotation.
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        spin = false;
     }
 
-    // Update is called once per frame
+    // Update checks what needs to be done to the tile in each frame
     void Update()
     {
-        
+        if (spin)
+            rotateHex();
     }
 
-    Vector3 getPosition()
+    public Vector3 getPosition()
     {
+        //Debug.Log(transform.position);
         return transform.position;
+    }
+
+    void rotateHex()
+    {
+        angleCount++;
+        transform.localEulerAngles = new Vector3(-angleCount, 0, 0);
+        if (angleCount == 360)
+        {
+            spin = false;
+            angleCount = 0;
+        }
+    }
+
+    public void spinTile()
+    {
+        spin = true;
     }
 }
