ca43342d23a184c9b18be3d957691be1af788445
Viktor Carlsson <99viktor.vc@gmail.com>
2021-04-26 10:43:11 +0200

    Fixat startmeny

---
+++ Assets/Scripts/MenuScript.cs
@@ -0,0 +1,46 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using Valve.VR;
+
+public class MenuScript : MonoBehaviour
+{
+    public LineRenderer lineRender;
+    public GameObject activeTarget;
+    public Transform source;
+    // Start is called before the first frame update
+    void Start()
+    {
+        lineRender = GetComponent<LineRenderer>();
+        source = GetComponent<Transform>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        lineRender.SetPosition(0, source.position);
+        lineRender.SetPosition(1, source.position + source.TransformDirection(Vector3.forward) * 10);
+        RaycastHit hit;
+        if (Physics.Raycast(transform.position, transform.TransformDirection(Vector3.forward), out hit))
+        {
+            activeTarget = hit.transform.gameObject;
+            RegisterButtonEvents buttonEvent = activeTarget.GetComponent<RegisterButtonEvents>();
+            if (buttonEvent != null)
+            {
+                buttonEvent.RegisterHover();
+                if (SteamVR_Actions.default_GrabPinch.GetStateDown(SteamVR_Input_Sources.Any)) buttonEvent.RegisterClick();
+                lineRender.SetPosition(0, source.position);
+                lineRender.SetPosition(1, source.position + source.TransformDirection(Vector3.forward) * hit.distance);
+            }
+        }
+        else clearTarget();
+    }
+
+    void clearTarget()
+    {
+        RegisterButtonEvents buttonEvent = activeTarget.GetComponent<RegisterButtonEvents>();
+        if (buttonEvent) buttonEvent.RemoveHover();
+        activeTarget = null;
+    }
+}
---
+++ Assets/Scripts/RegisterButtonEvents.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class RegisterButtonEvents : MonoBehaviour
+{
+    public Button button;
+
+    void Start()
+    {
+        button = GetComponent<Button>();
+    }
+
+    public void RegisterHover()
+    {
+        button.image.color = button.colors.highlightedColor;
+        Debug.Log("ayo");
+    }
+    public void RegisterClick()
+    {
+        button.onClick.Invoke();
+    }
+    public void RemoveHover()
+    {
+        button.image.color = button.colors.normalColor;
+        Debug.Log("cya");
+    }
+
+    public void StartGame()
+    {
+        Debug.Log("this is where the game scene loads");
+    }
+    public void Options()
+    {
+        Debug.Log("this is where the game options change");
+    }
+    public void QuitGame()
+    {
+        Debug.Log("this is where the game quits");
+    }
+}
